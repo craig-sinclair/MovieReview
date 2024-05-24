12d32a26d2631b8c3705fb508da10009d75e4ad1
2766838S <2766838S@student.gla.ac.uk>
2024-05-24 12:27:37 +0100

    read more button

--- filmreview/static/js/index.js
+++ filmreview/static/js/index.js
@@ -1,3 +1,4 @@
+//Change movie rating's text based on the given movie's rating
 document.addEventListener('DOMContentLoaded', function() {
     var ratingElements = document.querySelectorAll('.index-movie-rating');
 
@@ -13,4 +14,28 @@ document.addEventListener('DOMContentLoaded', function() {
             ratingElement.style.color = 'red'; 
         }
     });
+});
+
+// Read more button for a movie's overview, or a person's biography, if exceeds character limit
+document.addEventListener("DOMContentLoaded", function() {
+    const overviewElement = document.getElementById('movie-overview');
+    const readMoreButton = document.getElementById('read-more-button');
+    const maxLength = 250;
+    const fullText = overviewElement.textContent.trim();
+
+    if (fullText.length > maxLength) {
+        const truncatedText = fullText.substring(0, maxLength) + '... ';
+        overviewElement.textContent = truncatedText;
+        readMoreButton.classList.remove('hidden');
+
+        readMoreButton.addEventListener('click', function() {
+            if (readMoreButton.innerText === 'Read more') {
+                overviewElement.textContent = fullText;
+                readMoreButton.innerText = 'Read less';
+            } else {
+                overviewElement.textContent = truncatedText;
+                readMoreButton.innerText = 'Read more';
+            }
+        });
+    }
 });
